10867b109202e8b258bb893dcaa294428f529f3e
Infinity.Corp <toneysix@hotmail.com>
2018-07-01 14:59:04 +0500

    some changes

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@ using namespace std;
 
 int main()
 {
-    if( )
-//    SetConsoleCP (1251);
-//    SetConsoleOutputCP(1251);
+    #if PLATFORM == PLATFORM_WINDOWS
+    SetConsoleCP( 1251 );
+    SetConsoleOutputCP( 1251 );
+    #endif
 
 	try
 	{
@@ -28,14 +29,23 @@ int main()
 		}
 		// create server
 		server server(io_services);
-		sleep( 1 );
+
+		#if PLATFORM == PLATFORM_MAC || PLATFORM == PLATFORM_UNIX
+		sleep( 1 ); // it might help, but no way
+		#endif
+
 		// wait until all thread will finished
 		std::cout << "Waiting for incoming connections \n";
 		thr_grp.join_all();
         while( true )
         {
-            std::cout << "Wrong start, please, restart the server \n";
+            std :: cout << "Wrong start, please, restart the server \n";
+            #if PLATFORM == PLATFORM_MAC || PLATFORM == PLATFORM_UNIX
             usleep( 50000 );
+            #endif
+            #if PLATFORM == PLATFORM_WINDOWS
+            Sleep( 500 );
+            #endif
         }
 	}
 	catch( std::exception& e )
